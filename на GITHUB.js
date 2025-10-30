cef751a260b608fc2096b86dbe3849b1f0254cd7
vladislavius <vladike01@gmail.com>
2025-10-30 14:30:19 +0300

    Update на GITHUB.js

--- на GITHUB.js
+++ на GITHUB.js
@@ -1,8 +1,4 @@
 
-    <script>
-
-
-  
         // Ваш Google Maps API ключ
         const GOOGLE_MAPS_API_KEY = 'AIzaSyA2dvaJ7FB8j_9tWZHNoKDnQ2pAQRd-lV0';
         const BOATS_DATA_URL = 'https://raw.githubusercontent.com/vladislavius/map/refs/heads/main/combined_boat_data_from_yml_and_csv.json';
@@ -2608,6 +2604,6 @@ polyline.setMap(map);
             }
         }, 5000);
   
-    </script>
+   
 
 
